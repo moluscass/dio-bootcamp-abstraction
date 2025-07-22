2384140f072d487cddfcb35f47f7fd5b6c433d7e
moluscass <lodr130697@gmail.com>
2025-07-22 01:35:38 -0300

    Cursos/Mentoria v0.2

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.time.LocalDate;
 public class Main {
   public static void main(String[] args) {
     Curso[] curso = new Curso[2];
-    curso[1] = new Curso();
-    curso[0] = new Curso();
+    for(int i = 0; i < curso.length; i++) { // <- Pra facilitar outras demonstracoes...
+      curso[i] = new Curso();
+    }
     
     curso[0].setTitulo("Java");
     curso[0].setDescricao("Abstração e encapsulamento");
@@ -14,22 +15,25 @@ public class Main {
     curso[1].setDescricao("Curso de Javascript");
     curso[1].setCargaHoraria(8);
     
-    System.out.println(curso[0]);
-    System.out.println(curso[1]);
+    for(int i = 0; i < curso.length; i++) {
+      System.out.println(curso[i]);
+    }
     
     Mentoria[] mentoria = new Mentoria[2];
-    mentoria[0] = new Mentoria();
-    mentoria[1] = new Mentoria();
+    for(int i = 0; i < mentoria.length; i++) {
+      mentoria[i] = new Mentoria();
+    }
     
     mentoria[0].setTitulo("Java");
     mentoria[0].setDescricao("Mentoria Java");
     mentoria[0].setData(LocalDate.now());
     
-    mentoria[1].setTitulo("Java");
-    mentoria[1].setDescricao("Mentoria Java");
+    mentoria[1].setTitulo("Javascript");
+    mentoria[1].setDescricao("Mentoria Javascript");
     mentoria[1].setData(LocalDate.now());
     
-    System.out.println(mentoria[0]);
-    System.out.println(mentoria[1]);
+    for(int i = 0; i < mentoria.length; i++) {
+      System.out.println(mentoria[i]);
+    }
   }
 }
